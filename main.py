d5dcff0adc28eef88b809f41cc6cda57e975487b
jp-superstar <jyotiprakash0608@gmail.com>
2021-06-24 23:08:45 +0530

    added my name

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app= Flask(__name__)
 
 @app.route('/',methods=['GET', 'POST'])
 def index():
-    return "<h1>Welcome to flask</h1>"
+    return "<h1>Welcome to flask, I am Jyotiprakash</h1>"
 if __name__=="__main__":
     app.run()
 
